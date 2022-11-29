9f9d2a6e566cda053ccbe030093b1cedb1659f3c
Danilo Melo <danilomarquesdemelo@gmail.com>
2022-11-29 13:40:17 -0300

    Implantado eventos do Modal e menu Drop-down com JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,30 @@
+const openModalBtnList = document.querySelectorAll(".new-item")
+const fade = document.querySelector(".fade")
+const modal = document.querySelector(".modal")
+const closeModalBtn = document.querySelector(".modal_btn-cancel")
+const listItems = document.querySelectorAll(".list-item") 
 
 
+
+// Modal and Fade
+openModalBtnList.forEach(btn => {
+    btn.addEventListener("click", () => {
+        fade.classList.toggle("hide")
+        modal.classList.toggle("hide")
+    })
+})
+
+closeModalBtn.addEventListener("click", () => {
+    fade.classList.toggle("hide")
+    modal.classList.toggle("hide")
+})
+
+
+// Drop-Down menu
+listItems.forEach(element => {
+    element.addEventListener("click", (event)=>{
+        if(event.target.classList.contains("list-item_name")){
+            event.target.nextElementSibling.classList.toggle("hide-list-options")
+        }
+    })
+})
