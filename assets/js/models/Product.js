d9149669f777f531389c54353d560af7ec2a2937
Danilo Melo <danilomarquesdemelo@gmail.com>
2022-12-21 10:06:26 -0300

    Criado model Product.js

---
+++ assets/js/models/Product.js
@@ -0,0 +1,35 @@
+class Product {
+
+    constructor(prodName, prodPrice, prodQuant){
+        this._prodName = prodName
+        this._prodPrice = prodPrice
+        this._prodQuant = prodQuant
+    }
+
+    get prodName(){
+        return this._prodName
+    }
+
+    get prodPrice(){
+        return this._prodPrice
+    }
+
+    get prodQuant(){
+        return this._prodQuant
+    }
+
+    get volume (){
+        return this._prodPrice * this._prodQuant
+    }
+}
+
+// const teste = new Product("arroz", 10, 2)
+
+// const banco = new LocalStorageDB()
+
+// banco.saveItemDB(teste)
+
+// console.log(banco.localStorageData);
+
+
+
