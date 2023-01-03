3db6a5a128eaae0b7ce7a5b2795bf27c8efde103
Danilo Melo <danilomarquesdemelo@gmail.com>
2023-01-03 11:53:08 -0300

    Implantado view com os dados do localStorage

--- assets/js/controllers/ProductController.js
+++ assets/js/controllers/ProductController.js
@@ -4,14 +4,23 @@ class ProductController{
         this._inputProdName = document.querySelector("#modal_item-name")
         this._inputProdPrice = document.querySelector("#modal_item-price")
         this._inputProdQquant = document.querySelector("#modal_item-quant")
+        
+        this._btnsAddItem = document.querySelectorAll(".new-item")
+        
+        this._viewListItems = document.querySelectorAll(".list-item")
 
         this._localStorageDB = new LocalStorageDB()
+
         this._productListView = new ProductListView(document.querySelector(".list"))
         this._productListView.update(this._localStorageDB)
+
+        // this._productDropDown = new ProductDropDown()
     }
 
     newProduct(event){
         event.preventDefault()
+
+        this._modalToogle()
         
         const product = new Product(this._inputProdName.value, this._inputProdPrice.value, this._inputProdQquant.value)  
         
@@ -19,7 +28,19 @@ class ProductController{
 
         this._productListView.update(this._localStorageDB)
 
-        // console.log(this._localStorageDB.localStorageData)
+        // this._productDropDown.dropDown()
+
+        this._productListView.dropDown(this._viewListItems)
+
+    }
+
+    _modalToogle(){
+        this._btnsAddItem.forEach(btn =>{
+            btn.addEventListener("click", ()=>{
+                // document.querySelector(".modal").classList.toogle("hide")
+                console.log("teste")
+            })
+        })
     }
 }
 
--- assets/js/views/ProductListView.js
+++ assets/js/views/ProductListView.js
@@ -1,15 +1,26 @@
-class ProductListView{
+class ProductListView {
 
-    constructor(elementHTML){
+    constructor(elementHTML) {
         this._elementHTML = elementHTML
     }
 
-    _template(model){
+    _template(model) {
+        return model.localStorageData.map(data => {
+            return `<div class="list-item">
+                        <p class="list-item_name">${data._prodName}</p>
+                        <div class="list-item_options hide-list-options">
+                            <div class="list-item_line"></div>
+                            <p class="list-item_price">Valor Unitário: <span>${data._prodPrice} R$</span></p>
+                            <p class="list-item_quant">Quantidade: <span>${data._prodQuant}</span></p>
+                            <button class="btn-edit"><i class="fa-solid fa-pen"></i>Editar</button>
+                            <button class="btn-delete"><i class="fa-regular fa-trash-can"></i></button>
+                        </div>
+                    </div>`
+        }).join("")
 
+    }
 
-    }  
-
-    update(model){
+    update(model) {
         this._elementHTML.innerHTML = this._template(model)
     }
 }
